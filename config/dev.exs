564d6e74808cfa5394ebe99ef820424c111fc09e
Andrew Moll <mollandrew913@gmail.com>
2016-11-11 15:34:56 +0800

    Changed the username and password

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :pxblog, Pxblog.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "drew",
+  password: "drew",
   database: "pxblog_dev",
   hostname: "localhost",
   pool_size: 10
